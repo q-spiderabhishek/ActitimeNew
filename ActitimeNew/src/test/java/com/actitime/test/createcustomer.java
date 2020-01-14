aad99da06a6aef9c7005ec9815c924bd63fc94f3
Abhishek <Abhishek@DESKTOP-0FM1T26>
2020-01-14 11:52:56 +0530

    first commit

---
+++ ActitimeNew/src/test/java/com/actitime/test/createcustomer.java
@@ -0,0 +1,14 @@
+package com.actitime.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class createcustomer {
+@Test
+	public void testcreatecustomer() 
+	{
+		Reporter.log("create customer",true);
+		
+	}
+
+}
